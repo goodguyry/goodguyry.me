91895b6c754aa42d2f05bef383d5d8d41bf9ae6b
Ryan Domingue <ryan@ryandomingue.com>
2016-02-26 23:08:10 -0800

    Adds Sass NPM script

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task('scripts', function() {
+gulp.task('default', function() {
   return gulp.src('./_loadCSS/loadCSS.js')
     .pipe(uglify())
     .pipe(rename({
@@ -10,24 +10,3 @@ gulp.task('scripts', function() {
     }))
     .pipe(gulp.dest('./_includes/'));
 });
-
-var sass = require('gulp-sass');
-var gulpNano = require('gulp-cssnano');
-
-var nanoOpts = {
-  autoprefixer : {
-    add : true
-  }
-};
-
-gulp.task('styles', function () {
-  return gulp.src('./_scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulpNano(nanoOpts))
-    .pipe(rename({
-      suffix : ".min"
-    }))
-    .pipe(gulp.dest('./css/'));
-});
-
-gulp.task('default', ['scripts', 'styles']);
