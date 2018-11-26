00bf624069798f0725b159d363255a5a6593d666
Ryan Domingue <rychdom+github@gmail.com>
2018-11-25 16:21:52 -0800

    Sets up projects component

--- _client/entries/base/index.js
+++ _client/entries/base/index.js
@@ -1,5 +1,6 @@
 import '../../components/global-header';
 import '../../components/global-nav';
+import '../../components/projects';
 import '../../components/global-footer';
 import '../../../js/main';
 import '../../../css/base.scss';
