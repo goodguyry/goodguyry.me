ad131aafe9d35b23abffd79d9621b8446b79af1f
Ryan Domingue <rychdom+github@gmail.com>
2018-12-01 13:07:49 -0800

    Cleans up critical script

--- _build-scripts/critical.js
+++ _build-scripts/critical.js
@@ -1,45 +1,61 @@
-var penthouse = require('penthouse');
-var nano = require('cssnano');
-var fs = require('fs');
+const penthouse = require('penthouse');
+const nano = require('cssnano');
+const fs = require('fs');
+const path = require('path');
+const readYaml = require('read-yaml');
+const paths = require('../_client/config/paths');
+
+/**
+ * Get the hashed filename for a given asset.
+ *
+ * @param {String} name The unhashed filename.
+ */
+function getHashedAssets(name) {
+  const [asset, ext] = name.split('.');
+  const assets = readYaml.sync(
+    path.join(paths.siteData, 'assets.yaml')
+  );
+
+  return assets[asset][ext];
+}
 
 // Strings for different templates
-var templates = {
+const templates = {
   home: {
     url: 'http://goodguyry.http',
-    css: './_site/css/base.css',
-    forceInclude: [/^nav\s*/, /footer\s*/],
-    outfile: './_includes/critical-home.html'
+    css: `./build/${getHashedAssets('base.css')}`,
+    forceInclude: [/^\.global-nav__*/, /\.global-footer__*/],
+    outfile: './_includes/critical-home.html',
   },
   post: {
     url: 'http://goodguyry.http/notes/multi-tenant-wordpress.html',
-    css: './_site/css/base.css',
-    forceInclude: [/^nav\s*/],
-    outfile: './_includes/critical-post.html'
-  }
+    css: `./build/${getHashedAssets('base.css')}`,
+    forceInclude: [/^\.global-nav__*/],
+    outfile: './_includes/critical-post.html',
+  },
 };
 
-// Loop through the templates and generate criticalCSS for each
-Object.keys(templates).forEach(function (key) {
+/**
+ * Generate criticalCSS for each template.
+ */
+Object.keys(templates).forEach((key) => {
   penthouse({
     url: templates[key].url,
     css: templates[key].css,
     width: 720,
     height: 800,
     forceInclude: templates[key].forceInclude,
-    timeout: 30000
-  }, function (err, critical) {
+    timeout: 30000,
+  }, (err, critical) => {
     if (err) {
-      // Handle errors
       throw err;
     } else {
       // Minify the output
       nano.process(critical, {
-        autoprefixer: {
-          add: true // Add vendor prefixes
-        }
-      }).then(function (result) {
-        // Wrap output in <style> tags
-        fs.writeFileSync(templates[key].outfile, '<style>' + result.css + '</style>');
+        autoprefixer: { add: false },
+      }).then((result) => {
+        // Wrap output in <style></style>
+        fs.writeFileSync(templates[key].outfile, `<style>${result.css}</style>`);
       });
     }
   });
