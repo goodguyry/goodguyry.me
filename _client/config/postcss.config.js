2726ce74f47583548322128783780f1d5ad4e704
Ryan Domingue <rychdom+github@gmail.com>
2020-01-25 11:03:33 -0500

    Adds tidy-columns

--- _client/config/postcss.config.js
+++ _client/config/postcss.config.js
@@ -3,12 +3,24 @@ const fs = require('fs');
 const readYaml = require('read-yaml');
 const autoprefixer = require('autoprefixer');
 const modules = require('postcss-modules');
+const columns = require('postcss-tidy-columns');
 const paths = require('./paths');
 const yamlDictFromObject = require('./bin/yamlDictFromObject');
 
 // Config
 module.exports = () => ({
   plugins: [
+    columns({
+      columns: 8,
+      gap: '0.375rem',
+      edge: '1.25rem',
+      breakpoints: {
+        '48rem': {
+          gap: '1rem',
+          siteMax: '39.625rem',
+        },
+      },
+    }),
     autoprefixer(),
     modules({
       generateScopedName: '[name]__[local]___[hash:base64:5]',
