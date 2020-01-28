e0e17299809925cf95badf913d9c2b18348fc8b5
Ryan Domingue <rychdom+github@gmail.com>
2020-01-27 21:46:22 -0800

    Adds the archive entry

--- _client/config/modules/entry.js
+++ _client/config/modules/entry.js
@@ -2,5 +2,6 @@ module.exports = {
   global: '_client/entries/global',
   home: '_client/entries/home',
   post: '_client/entries/post',
+  archive: '_client/entries/archive',
   code: '_client/entries/code',
 };
