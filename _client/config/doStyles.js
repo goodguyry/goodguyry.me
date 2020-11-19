da44f3e9b67e443b9401125af817e71f4ae058c4
Ryan Domingue <rychdom+github@gmail.com>
2020-11-18 21:02:35 -0800

    Removes CSS Modules

--- _client/config/doStyles.js
+++ _client/config/doStyles.js
@@ -4,12 +4,10 @@ const fs = require('fs');
 const sass = require('sass');
 const postcss = require('postcss');
 const units = require('postcss-units');
-const modules = require('postcss-modules');
 const tidyColumns = require('postcss-tidy-columns');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const paths = require('./paths');
-const yamlDictFromObject = require('./bin/yamlDictFromObject');
 
 /**
  * Process Sass styles.
@@ -36,34 +34,6 @@ module.exports = async (inputFile) => {
           gap: '1rem',
           siteMax: '39.625rem',
         },
-      },
-    }),
-    modules({
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
-      globalModulePaths: [
-        /_client\/src\/scss\/global/,
-        /_client\/entries\/code/,
-      ],
-      getJSON: (cssFileName, json) => {
-        const { name } = path.parse(cssFileName);
-        let modulesMap;
-
-        if (0 < Object.keys(json).length) {
-          try {
-            modulesMap = JSON.parse(fs.readFileSync(
-              path.join(paths.siteData, 'classnames.json'),
-              'utf8'
-            ));
-          } catch (error) {
-            modulesMap = {};
-          }
-
-          modulesMap[name] = json;
-          fs.writeFileSync(
-            path.join(paths.siteData, 'classnames.json'),
-            JSON.stringify(yamlDictFromObject(modulesMap))
-          );
-        }
       },
     }),
     autoprefixer(),
