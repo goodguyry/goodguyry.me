a046f61c3ac89dea52d449c107bb26bfab8738c3
Ryan Domingue <rychdom+github@gmail.com>
2020-11-25 22:47:28 -0800

    Removes extraneous config.paths properties

--- _client/config/paths.js
+++ _client/config/paths.js
@@ -4,13 +4,6 @@ const projectRoot = path.join(__dirname, '../../');
 
 module.exports = {
   projectRoot,
-  site: path.join(projectRoot, '_site'),
   public: path.join(projectRoot, '_site'),
-  components: path.join(projectRoot, '_client/components'),
   scss: path.join(projectRoot, '_client/src/scss'),
-  js: path.join(projectRoot, '_client/src/js'),
-  fonts: path.join(projectRoot, '_client/src/fonts'),
-  entries: path.join(projectRoot, '_client/entries'),
-  siteData: path.join(projectRoot, '_data'),
-  config: __dirname,
 };
