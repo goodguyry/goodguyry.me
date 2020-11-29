b8cbfaa8a1695a9159670fd8000f6189a38f7a63
Ryan Domingue <rychdom+github@gmail.com>
2020-11-29 12:39:45 -0800

    Updates environment checks

--- _client/data/site.js
+++ _client/data/site.js
@@ -1,12 +1,25 @@
 const environment = process.env.ELEVENTY_ENV;
-const isDev = ('development' === environment);
+let domain = 'http://goodguyry.me';
+
+switch (environment) {
+  case 'local':
+    domain = 'http://goodguyry.http';
+    break;
+  case 'dev':
+    domain = 'http://dev.goodguyry.me';
+    break;
+}
 
 module.exports = {
-  environment,
   title: 'Ryan Domingue is a UX Developer living in Seattle, Washington',
   description: 'Ryan Domingue bends markup and CSS to his will, and writes as much or as little JavaScript as needed.',
   blogroll: '/notes/',
-  domain: `http://goodguyry.${isDev ? 'http' : 'me'}`,,
+  domain,
   username: 'goodguyry',
   themeColor: '#008583',
+  env: {
+    isLocal: ('local' === environment),
+    isDev: ('dev' === environment),
+    isDotMe: ('dotMe' === environment),
+  },
 };
